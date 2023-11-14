ed6ceea05305a1570b444a06a9fb25df98bc49ee
陳泰恩 <rjoeyic@gmail.com>
2023-11-14 21:41:37 +0800

    設定Route前置

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,11 @@ Route::get('r2', function () {
 Route::get('hello/{name?}', function ($name = 'Everybody') {
     return 'hello ,' . $name;
 });
+Route::get('dashboard', function () {
+    return 'dashboard';
+});
+Route::group(['prefix' => 'admin'], function () {
+    Route::get('dashboard', function () {
+        return 'admin dashboard';
+    });
+});
